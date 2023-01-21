80bc977503ffee5a01d363b6c29eb739b1a4249a
Gabriel Serafim <68358029+Gaserafim@users.noreply.github.com>
2023-01-21 12:25:11 -0300

    Update ask_to_gpt.py
    Atualização de comentários.

--- ask_to_gpt.py
+++ ask_to_gpt.py
@@ -1,14 +1,10 @@
 import openai
 import os
 
-# Set the API key
-openai.api_key = os.environ.get('OPENAI_API_KEY')
+openai.api_key = os.environ.get('OPENAI_API_KEY') # Definir a variável de ambiente com a API_KEY gerada no site do ChatGPT.
 
-# Define the prompt
 prompt = input('Digite sua pergunta: ')
 
-# Generate text
 response = openai.Completion.create(engine="text-davinci-002", prompt=prompt)
 
-# Print the generated text
 print(response["choices"][0]["text"])
